5a974e731f848868042e5bd1460a6b36b97d42e2
椎名潤七 <newbingtochatgpt233@outlook.com>
2025-03-05 18:27:10 +0800

    初始化

---
+++ src/main/kotlin/io/github/sciencekex/booksmanagementsystemrewrite/BooksManagementSystemRewriteApplication.kt
@@ -0,0 +1,11 @@
+package io.github.sciencekex.booksmanagementsystemrewrite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BooksManagementSystemRewriteApplication
+
+fun main(args: Array<String>) {
+    runApplication<BooksManagementSystemRewriteApplication>(*args)
+}
